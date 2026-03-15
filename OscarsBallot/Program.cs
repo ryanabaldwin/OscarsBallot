88e620274eb67045f3d81e6c38005b7b98660096
ryanabaldwin <ryanb24@byu.edu>
2026-03-14 21:43:19 -0600

    Changes to Program.cs

--- OscarsBallot/Program.cs
+++ OscarsBallot/Program.cs
@@ -3,7 +3,7 @@ using System;
 using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using OscarsBallot.Data; //
+using OscarsBallot.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
